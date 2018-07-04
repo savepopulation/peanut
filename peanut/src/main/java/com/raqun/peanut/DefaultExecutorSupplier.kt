1cda78a208c9369222285f241fa830f5f9e31068
tyln <taylan.sbrcn@gmail.com>
2018-07-04 22:47:33 +0300

    fixed possible io thread bug

--- peanut/src/main/java/com/raqun/peanut/DefaultExecutorSupplier.kt
+++ peanut/src/main/java/com/raqun/peanut/DefaultExecutorSupplier.kt
@@ -8,7 +8,7 @@ import java.util.concurrent.Executors
  * Created by tyln on 13.06.2018.
  */
 object DefaultExecutorSupplier : ExecutorSupplier {
-    private val MAX_IO_THREAD_COUNT = Util.getNumberOfCores()
+    private val MAX_IO_THREAD_COUNT = calculateMaxIThreadCount()
 
     // Executors
     private val mainThreadExecutor: Executor = MainThreadExecutor()
@@ -17,4 +17,9 @@ object DefaultExecutorSupplier : ExecutorSupplier {
     override fun provideMainExecutor(): Executor = mainThreadExecutor
 
     override fun provideIoExecutor(): Executor = ioExecutor
+
+    private fun calculateMaxIThreadCount(): Int {
+        val calculatedIoThreadCount = Util.getNumberOfCores()
+        return if (calculatedIoThreadCount < 2) 2 else calculatedIoThreadCount
+    }
 }
